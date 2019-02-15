1e7dc769c196ac739153cc0a576aafc373120f2c
epwalsh <epwalsh10@gmail.com>
2019-02-15 07:37:21 -0800

    Fix

--- petesapp/test/glove_test.py
+++ petesapp/test/glove_test.py
@@ -4,5 +4,5 @@ from petesapp.glove import encode_word
 def test_encode_word():
     vector = encode_word("hello")
     assert isinstance(vector, list)
-    assert len(vector) == 100
+    assert len(vector) == 50
     assert isinstance(vector[0], float), type(vector[0])
